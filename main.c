edbe3195b1644a54a0c824d5f345ea377d71eeb0
Brodacky_Marek <brodackym@gmail.com>
2021-04-26 14:05:36 +0200

    test function, btree_search, node_insert

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 //Node struct
 typedef struct node {
     int *keys; // array of keys (size 2t-1)
-    int *children; // array of pointers to children (size 2t)
+    int **children; // array of pointers to children (size 2t)
     int parent; // pointer to parent
-    int n; // number of key in node
+    int n; // number of keys in node
     int leaf; // it node leaf? - values 0(false)/ 1(true)
 }node;
 
@@ -35,6 +35,30 @@ int node_is_root(btree *tree, node *node){
     return 0;
 }
 
+void node_insert(node *n, int key){
+    int insert_index = -1;
+    for(int j = 0; j < n->n; j++){
+        if(key < n->keys[j]){
+            insert_index = j;
+            break;
+        }
+    }
+
+    if(insert_index == -1){
+        n->keys[n->n] = key;
+        printf("n: %i", n->keys[n->n]);
+        n->n += 1;
+    } else {
+        n->n += 1;
+        int temp = key;
+        for(int i = insert_index; i < n->n; i++){
+            int t = n->keys[i];
+            n->keys[i] = temp;
+            temp = t;
+        }
+    }
+}
+
 //Constructor to create new B-tree
 void btree_constructor(btree *tree, int t){
     if(t < 2){
@@ -48,32 +72,85 @@ void btree_constructor(btree *tree, int t){
     tree->root = 0;
 }
 
+void btree_insert(btree *tree, int key){
+    node *last_node;
+
+}
+
+int btree_search(node *n, node **last_node, int key){
+    int i = 0;
+    while(i < n->n && key > n->keys[i])
+        i++;
+
+    if(i < n->n && n->keys[i] == key){
+        *last_node = n;
+        return i;
+    }
+
+    if(n->leaf){
+        *last_node = n;
+        return -1;
+    }
+
+    return btree_search(n->children[i], last_node, key);
+}
+
 //Print informations of the B-tree
 void btree_print(btree *tree){
     printf("\n###B-tree: t: %i, min: %i, max: %i, &root: %i###", tree->t, tree->min, tree->max, tree->root);
 }
 
 //Print informations of the node
-void node_print(node *node){
-    printf("\n#Node: &keys: [");
-    for(int j = 0; j < node->n; j++)
-        printf("%i ", &node->keys[j]);
-    printf("], &children:[");
-    if(node->n > 0)
-        for(int j = 0; j <= node->n; j++)
-            printf("%i ", &node->children[j]);
-    printf("], &parent: %i, n: %i, leaf: %i#", node->parent, node->n, node->leaf);
+void node_print(node *n){
+    printf("\n#Node(&%i): keys: [ ", n);
+    for(int j = 0; j < n->n; j++)
+        printf("%i ", n->keys[j]);
+    printf("], &children:[ ");
+    if(!n->leaf)
+        for(int j = 0; j <= n->n; j++)
+            printf("%i ", n->children[j]);
+    printf("], &parent: %i, n: %i, leaf: %i#", n->parent, n->n, n->leaf);
+}
+
+void test(){
+    int t = 2;
+
+    btree strom;
+    btree_constructor(&strom, t);
+
+    node uzel_root;
+    node_constructor(&strom, &uzel_root, 0);
+    node_insert(&uzel_root, 26);
+
+    node uzel_1;
+    node_constructor(&strom, &uzel_1, 0);
+    node_insert(&uzel_1, 6);
+    node_insert(&uzel_1, 12);
+
+    node uzel_2;
+    node_constructor(&strom, &uzel_2, 0);
+    node_insert(&uzel_2, 42);
+    node_insert(&uzel_2, 51);
+    node_insert(&uzel_2, 62);
+
+    strom.root = &uzel_root;
+    uzel_root.children[0] = &uzel_1;
+    uzel_root.children[1] = &uzel_2;
+    uzel_root.leaf = 0;
+
+    btree_print(&strom);
+    node_print(&uzel_root);
+    node_print(&uzel_1);
+    node_print(&uzel_2);
+
+    node *test;
+    int key = btree_search(&uzel_root, &test, 62);
+
+    printf("\naddress:%i, key:%i", test, key);
 }
 
 int main()
 {
-    btree strom;
-    int t = 2;
-    btree_constructor(&strom, t);
-    btree_print(&strom);
-
-    node uzel;
-    node_constructor(&strom, &uzel, 0);
-    node_print(&uzel);
+    test();
     return 0;
 }
